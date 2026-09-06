7b3fb5bbd4797bd380524c7548e8925cdafcbc65
James Radtke <cloudxabide@gmail.com>
2026-09-06 08:09:43 -0400

    generic update

--- Scripts/post_install.sh
+++ Scripts/post_install.sh
@@ -11,7 +11,9 @@ sudo systemctl enable sshd --now
 DAUSER=mansible
 echo "$DAUSER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$DAUSER-nopasswd-all
 
-
+# NOTE! NOTE! NOTE!
+# NOTE! NOTE! NOTE!
+# this issue is a Work In Progress still - the "fix" is to simply disable firewalld
 # firewalld on RHEL 10 defaults to the nftables backend, which conflicts with
 # RKE2's Canal CNI (flannel + Calico policy, which programs iptables-nft rules
 # for pod-to-pod traffic). Left as nftables, firewalld's own FORWARD-chain
@@ -19,25 +21,27 @@ echo "$DAUSER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$DAUSER-nopassw
 # is correct -- symptom is "no route to host" between pods on the same node
 # (e.g. a multi-replica Rancher install failing HA peer connections). Doing
 # this up front avoids having to catch it later.
-sudo sed -i 's/^FirewallBackend=.*/FirewallBackend=iptables/' /etc/firewalld/firewalld.conf
-sudo systemctl restart firewalld
+#sudo sed -i 's/^FirewallBackend=.*/FirewallBackend=iptables/' /etc/firewalld/firewalld.conf
+#sudo systemctl restart firewalld
+sudo systemctl disable firewalld --now
+
+
+## Connect to RHC for updates, etc...
 RHC_USERNAME=
 RHC_PASSWORD=
 sudo rhc connect --username "$RHC_USERNAME" --password "$RHC_PASSWORD"
+sudo subscription-manager status
+sudo subscription-manager refresh
+sudo subscription-manager release --list      # forces content-cert regen
+cat /etc/yum.repos.d/redhat.repo | head -30    # should now be hundreds of lines
+sudo subscription-manager repos --list 
+sudo dnf repolist
 
-  sudo subscription-manager status
-  sudo subscription-manager refresh
-  sudo subscription-manager release --list      # forces content-cert regen
-  cat /etc/yum.repos.d/redhat.repo | head -30    # should now be hundreds of lines
-  sudo subscription-manager repos --list 
-  sudo dnf repolist
-
-# There seems to be an issue with Red Hat's CDN (or mirrors?) when I tried this - so... try the first command, then the 2nd if the first does not work
+# I encountered an issue with Red Hat's CDN (or mirrors?) when I tried this - so... try the first command, then the 2nd if the first does not work
 sudo dnf update -y
 
-# The following will keep trying (and eventually succeed)
+# The following will keep trying (and eventually succeed) - You will need to quit (CTRL-C) once evertyhing is updated)
 while true; do sudo dnf update -y; sleep 5; done
-# You will need to quit (CTRL-C) once evertyhing is updated)
 
 # 1. Enable CodeReady Builder (EPEL expects it)
 sudo subscription-manager repos --enable codeready-builder-for-rhel-10-x86_64-rpms
